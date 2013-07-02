6f24cebb83fac2475e595814a4ed487a45342db7
Matt Mawhinney <matt.mawhinney@gmail.com>
2013-07-02 18:06:26 -0400

    rubeque & website

--- Ruby/each_with_object_enumerable.rb
+++ Ruby/each_with_object_enumerable.rb
@@ -1,10 +1,18 @@
-# Not working yet 
+# http://ruby-doc.org/core-2.0/Enumerable.html#method-i-each_with_object
+# Returns the reverse of words in a given array with an even number of chars
+# using Enumerable#each_with_object
+# This can be used instead of creating some new array using an array literal 
+# to store values created by iterating over some type of object 
 
-def even_sum(arr) 
+  def even_sum(arr)
+    arr.compact.each_with_object([]) do |word, array| 
+      if word.length % 2 == 0     
+        array << word.reverse  
+      end
+    end 
+  end  
 
-  arr.each_with_object([]) { |x, a|  a << x.reverse if x.length % 2 == 0 }  
+ puts even_sum(["cat", "dog", "bird", "fish"]) == ["drib", "hsif"]
+ puts even_sum(["why", "chunky", nil, "lucky", "stiff"]) == ["yknuhc"]
+ puts even_sum(["rewsna", "hitch hiker", "si", "guide", "galaxies ", "24"]) == ["answer", "is", "42"]
 
-end
-
-
-even_sum(['boat','dog','fish'])
